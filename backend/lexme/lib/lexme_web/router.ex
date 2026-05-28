eefa7cb0a38d573cd804641444719c0046657a8e
majaantovic00-coder <majaantovic00@gmail.com>
2026-05-28 09:36:49 +0200

    Revert "Merge pull request #5 from majantoviccc/backend"
    This reverts commit ba4d41e500e28ae83e7678fdb0ea88d3b862afe2, reversing
    changes made to 5e25a726bd111b9fb07127daf5f3f9e367c6cc40.

--- backend/lexme/lib/lexme_web/router.ex
+++ backend/lexme/lib/lexme_web/router.ex
@@ -20,13 +20,10 @@ defmodule LexmeWeb.Router do
     get "/", PageController, :home
   end
 
-  scope "/api", LexmeWeb do
-    pipe_through :api
-
-    resources "/projects", ProjectController, only: [:index, :show, :create, :delete]
-    resources "/threads", ThreadController, only: [:index, :show, :create, :delete]
-    resources "/messages", MessageController, only: [:index, :create, :delete]
-  end
+  # Other scopes may use custom stacks.
+  # scope "/api", LexmeWeb do
+  #   pipe_through :api
+  # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:lexme, :dev_routes) do
